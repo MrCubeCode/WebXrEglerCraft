dc1ee8241096db8eab2974f52be39e9422f32c6a
MrCubeCode <155542202+MrCubeCode@users.noreply.github.com>
2024-07-01 11:39:31 +0700

    Update VRmod.js

--- js/VRmod.js
+++ js/VRmod.js
@@ -1,7 +1,15 @@
-setInterval(mode,1000);
+setInterval(mode,100);
 function mode() {
 //  console.log("g");
-  document.getElementsByTagName('canvas')[0].setAttribute("id", "canvas");
+ // document.getElementsByTagName('canvas')[0].setAttribute("id", "canvas");
+   document.createElement('canvas');
+  var sourceCanvas = document.getElementsByTagName('canvas')[0];
+  var sourceCanvas = destinationCanvas.getElementsByTagName('canvas')[1];
+  let srcCtx = sourceCanvas.getContext('2d');
+let destCtx = destinationCanvas.getContext('2d');
+
+destCtx.drawImage(sourceCanvas, 0, 0);
+  
 // var g = document.createElement('canvas');
 // g.setAttribute("id", "canvas");
 //   document.body.appendChild(g);
