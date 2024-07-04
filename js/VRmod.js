c39b41a0756eae9928adc173da4f7abe919ff5f4
MrCubeCode <155542202+MrCubeCode@users.noreply.github.com>
2024-07-04 11:51:29 +0700

    Update VRmod.js

--- js/VRmod.js
+++ js/VRmod.js
@@ -6,6 +6,7 @@ function mode() {
 //  console.log("g");
 
  var canvas = document.getElementsByTagName('canvas')[0];
+ canvas.setAttribute("id", "canvas");
  canvas.width = 30;
         canvas.height = 30;
 
